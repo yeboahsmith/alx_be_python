c17c1a8af5ccb37351c6613235d9cec503a87943
yeboahsmith <michaelsmithyeboah@gmail.com>
2025-08-07 00:10:33 +0000

    Update bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,2 +1,24 @@
 
+class BankAccount:
+  def __init__ (self, account_balance):
+    self.account_balance = account_balance
+    self.account_balance = 0
+
+def deposit (amount):
+  amount += self.account_balance
+  return amount
+
+def withdraw (with_amount):
+  WithdrawAmount = self.account_balance - with_amount
+  if WithdrawAmount < 0:
+    return True
+  else:
+      return False
+
+ def display_balance():
+   print(" Your current balance is: " + self.account_balance )
+   
+    
+    
+
 
